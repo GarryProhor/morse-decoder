bab2d5f7d2e321afbc376aa68afd3ab80f022ad5
igorp <igorprohorchenko@gmail.com>
2023-08-08 10:10:43 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,22 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let array = [];
+    const arrayWithoutZero = [];
+    const result = [];
+
+    for (let i = 0; i < expr.length; i+=10) {
+        array.push(expr.slice(i, i + 10));
+    }
+    array.forEach(item => item==='**********' ? arrayWithoutZero.push('*') : arrayWithoutZero.push(item.match(/(((10)|(11))+)/)[0]))
+
+    array = [];   // empty array for array with morse symbols
+    arrayWithoutZero.forEach(item => {
+        array.push(item.replace(/(10)/g, '.').replace(/(11)/g, '-'));
+    })
+
+    array.forEach(item => typeof(MORSE_TABLE[item]) == 'undefined' ? result.push(' ') : result.push(MORSE_TABLE[item]))
+    return result.join('');
 }
 
 module.exports = {
